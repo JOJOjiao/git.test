09907583b8b516020af2d51bd4e7fabcf60515de
Layne <Layne@atguigu.com>
2022-03-11 21:38:40 +0800

    third commit push

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args){
         System.out.println("Hello git!1");
         System.out.println("Hello git!2");
+        System.out.println("Hello git!3 push");
     }
 }
